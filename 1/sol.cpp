f1d9e30ef4c516c08e0efbf9e555429d59e74cf3
Thomas-Malcolm <ThomasMalcolm01@gmail.com>
2020-12-03 02:10:21 +1000

    day 1 part 2

--- 1/sol.cpp
+++ 1/sol.cpp
@@ -29,6 +29,44 @@ solve(vector<int>& nums, int want)
 
 }
 
+
+/*
+ * Three part sum
+ */
+int
+part2(vector<int>& nums, int want)
+{
+	map<int, pair<int, int>> sumMap;
+	map<int, int> m;
+
+	for (int i : nums) {
+		if (m.find(i) == m.end())
+			m[i] = 0;
+	}
+
+	for (int i : nums) {
+		pair<int, int> v;
+		v.first = i;
+
+		for (int j : nums) {
+			v.second = j;
+			if (sumMap.find(i + j) == sumMap.end())
+				sumMap[i + j] = v;
+		}
+	}
+
+	for (const auto [i, v] : sumMap) {
+		int x = want - i;
+
+		if (m.find(x) != m.end())
+			return x * v.first * v.second;
+	}
+
+	return -1;
+
+}
+
+
 int
 main(int argc, char **argv)
 {
@@ -39,11 +77,14 @@ main(int argc, char **argv)
 
 	int x;
 
+	f >> x;
+
 	do {
 		nums.push_back(x);
 	} while(f >> x);
 
-	int sol = solve(nums, 2020);
+	//int sol = solve(nums, 2020);
+	int sol = part2(nums, 2020);
 
 	cout << "Solution: " << sol << endl;
 
