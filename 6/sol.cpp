b0daa66e64a3e0fea9cda3a421eeea7fa5ecc3d7
Thomas-Malcolm <ThomasMalcolm01@gmail.com>
2020-12-16 13:42:23 +1000

    d6p2

--- 6/sol.cpp
+++ 6/sol.cpp
@@ -2,18 +2,78 @@
 #include <vector>
 #include <fstream>
 #include <string>
-
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
+struct group {
+	vector<string> people;
+};
+
 
 int
-part_one(vector<string> groups)
+part_one(vector<struct group> groups)
 {
 	int count = 0;
 
-	for (string g : groups) {
-		count += g.length();
+	vector<string> answers; /* strings of unique answers for each group */
+
+	for (struct group g : groups) { /* disgusting - was neater before the second part... */
+		string unique;
+
+		for (string person : g.people) { /* iterate through all the people's answers in a group */
+			for (char c : person) {
+				if (unique.find(string(1, c)) == string::npos) { /* if answer not already considered, count it */
+					unique += c;
+				}
+			}
+		}
+
+		answers.push_back(unique);
+	}
+
+	for (string a : answers) {
+		count += a.length();
+	}
+
+	return count;
+}
+
+int
+part_two(vector<struct group> groups)
+{
+	int count = 0;
+	string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	vector<string> answers;
+
+	/* To get shared answers, find the intersection of the individual sets of each person in the group's answers */
+	for (struct group g : groups) {
+		set<char> similar(alphabet.begin(), alphabet.end());
+
+		for (string person : g.people) {
+			set<char> per(person.begin(), person.end());
+			set<char> newSim;
+
+			//set_intersection(per.begin(), per.end(), similar.begin(), similar.end(), similar.begin(), similar.end()); /* requires sorted sets... */
+			/* manual set intersection it is then... */
+			for (char c : per) {
+				if (similar.count(c) > 0) {
+					newSim.insert(c);
+				}
+			}
+
+			similar = newSim;
+		}
+		string a(similar.begin(), similar.end());
+
+		answers.push_back(a);
+
+	}
+
+	for (string a : answers) {
+		count += a.length();
 	}
 
 	return count;
@@ -27,25 +87,28 @@ main(int argc, char** argv)
 	f.open("data.txt", ios_base::in);
 	string line;
 
-	vector<string> groups; /* store string of questions answered - unique letters*/
-	string group;
+	vector<struct group> groups; /* store string of questions answered - unique letters*/
+	vector<string> people;
+	struct group g;
 
 	while (getline(f, line)) {
 
 		if (line.length() == 0) {
-			groups.push_back(group);
-			group = "";
+			g.people = people;
+			groups.push_back(g);
+			g = {};
+			people = {};
 			continue;
 		}
 
-		for (char c : line) {
-			if (group.find(string(1, c)) == string::npos) {
-				group += c;
-			}
-		}
+		people.push_back(line);
 	}
 
-	cout << "Sum of counts: " << part_one(groups) << endl;
+	//while (getline(f, line)) data.push_back(line); /* get all the data... */
+
+	cout << "Part one: " << part_one(groups) << endl;
+
+	cout << "Part two: " << part_two(groups) << endl;
 
 
 	return 0;
